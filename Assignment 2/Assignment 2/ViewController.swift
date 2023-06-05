913444d62dd7ef5d8ae930f6391705b304dfaff3
jzg123321 <123005578+jzg123321@users.noreply.github.com>
2023-06-04 22:55:12 -0500

    Add files via upload

---
+++ Assignment 2/Assignment 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Josh Guiang on 6/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
